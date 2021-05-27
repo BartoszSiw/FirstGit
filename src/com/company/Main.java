8062025ff34e7922b68201ea0f520cd21a078f7c
BartoszSiw <82712022+BartoszSiw@users.noreply.github.com>
2021-05-27 13:53:17 +0200

    I added several lines in code.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
-import javax.swing.JFrame;
+import javax.swing.*;
+
 public class Main extends JFrame {
 
     public static void main(String[] args) {
         // write your code here
         Main gui = new Main();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setDefaultLookAndFeelDecorated(true);
         gui.setSize(500, 700);
         gui.setVisible(true);
         gui.setTitle("First Gui ");
+
+        JLabel labelM = new JLabel("How it's your name?");
+        labelM.setBounds(50, 50, 200, 30);
+        JTextField motto1 = new JTextField();
+        motto1.setBounds(50, 100, 200, 30);
     }
 
 }
